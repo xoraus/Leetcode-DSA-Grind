cdc6d97727db9c88984031c6802fa25decd4b300
Sajjad Salaria <imsafay@gmail.com>
2023-05-01 20:08:05 +0530

    Time: 5 ms (84.19%) | Memory: 48.2 MB (64.36%) - LeetSync

---
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int target) {
+        int rows = mat.length, cols = mat[0].length;
+        int row = 0, col = cols - 1;
+
+        while (row < rows && col >= 0) {
+            if (mat[row][col] == target)
+                return true;
+            if (mat[row][col] < target)
+                row++; // discard the topmost row
+            else
+                col--; // discard the rightmost column
+        }
+
+        return false;
+    }
+}
