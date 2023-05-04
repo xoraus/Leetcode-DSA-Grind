4efd53fdefca1b3f0279c213f0bf3a90bc2344c4
Sajjad Salaria <imsafay@gmail.com>
2023-05-04 22:36:20 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (32.49%) - LeetSync

---
+++ fibonacci-number/fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        if (n <= 1)
+            return n;
+        int a = 0, b = 1, c = 1;
+        for (int i = 2; i < n; i++) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return c;
+    }
+}
