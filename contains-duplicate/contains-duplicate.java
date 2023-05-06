d210a228f8f67817d5ef68c5a2fafb274e03e98a
Sajjad Salaria <imsafay@gmail.com>
2023-05-06 16:15:33 +0530

    Time: 20 ms (37.05%) | Memory: 56.6 MB (38.58%) - LeetSync

---
+++ contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
+        }
+        if(set.size()==nums.length){
+            return false;
+        }
+        return true;
+    }
+}
