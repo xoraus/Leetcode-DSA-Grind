4d047bfd715463995293d5dce60abb7cb795c645
Sajjad Salaria <imsafay@gmail.com>
2023-05-08 10:50:10 +0530

    Added solution - LeetHub

---
+++ Subarray with 0 sum - GFG/subarray-with-0-sum.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+			//code
+			
+		//taking input using class Scanner
+		Scanner scan = new Scanner(System.in);
+		
+		//taking total number of testcases
+		int t = scan.nextInt();
+		while(t>0)
+		{
+		    //taking total count of elements
+		    int n = scan.nextInt() ;
+		    
+		    //Declaring and Initializing an array of size n
+		    int arr[] = new int[n];
+		    
+		    //adding elements to the array
+		    for(int i = 0; i<n;i++)
+		    {
+		        arr[i] = scan.nextInt();
+		    }
+		    
+		    t--;
+		    
+		     //calling the method findSum
+		     //and print "YES" or "NO" based on the result
+		     System.out.println(new Solution().findsum(arr,n)==true?"Yes":"No");
+		}
+	}
+	
+	
+}
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to check whether there is a subarray present with 0-sum or not.
+    static boolean findsum(int arr[],int n) {
+        HashMap<Long, Integer> map = new HashMap<>();
+        map.put(0l, 1);
+        
+        long sum = 0l;
+        int count = 0;
+        
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            int freq = map.getOrDefault(sum, 0);
+            count += freq;
+            map.put(sum, freq+1);
+        }
+        
+        if(count >= 1) return true;
+        else return false;
+    }
+}
