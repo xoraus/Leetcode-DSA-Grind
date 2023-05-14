8e3ad0cb503a139db17c3e3f08745ba4b3f47b8b
Sajjad Salaria <imsafay@gmail.com>
2023-05-14 22:51:38 +0530

    Added solution - LeetHub

---
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.java
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+
+class MergeLists
+{
+    Node head;
+
+
+
+  /* Function to print linked list */
+   public static void printList(Node head)
+    {
+        
+        while (head!= null)
+        {
+           System.out.print(head.data+" ");
+           head = head.next;
+        }  
+        System.out.println();
+    }
+	
+	 
+ 
+     /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+       
+         
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			Node head1 = new Node(sc.nextInt());
+            Node tail1 = head1;
+            for(int i=0; i<n1-1; i++)
+            {
+                tail1.next = new Node(sc.nextInt());
+                tail1 = tail1.next;
+            }
+			Node head2 = new Node(sc.nextInt());
+            Node tail2 = head2;
+            for(int i=0; i<n2-1; i++)
+            {
+                tail2.next = new Node(sc.nextInt());
+                tail2 = tail2.next;
+            }
+			
+			LinkedList obj = new LinkedList();
+			Node head = obj.sortedMerge(head1,head2);
+			printList(head);
+			
+			t--;
+			
+         }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+  Merge two linked lists 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class LinkedList
+{
+    //Function to merge two sorted linked list.
+    Node sortedMerge(Node headA, Node headB) {
+        Node dummy = new Node(-1);
+        Node tail = dummy;
+
+        while (headA != null && headB != null) {
+            if (headA.data < headB.data) {
+                tail.next = headA;
+                tail = tail.next;
+                headA = headA.next;
+            } else {
+                tail.next = headB;
+                tail = tail.next;
+                headB = headB.next;
+            }
+        }
+
+        if (headA != null)
+            tail.next = headA;
+        else
+            tail.next = headB;
+        return dummy.next;
+   } 
+}
