3b15d216cf49de65350ca0c716fbc4ec5338a991
Sajjad Salaria <imsafay@gmail.com>
2023-05-15 19:16:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (38.12%) - LeetSync

--- copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,25 +15,40 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node dummy = new Node(-1);
-        Node l1 = head, l2 = dummy;
-        HashMap<Node, Node> mirror = new HashMap<>();
+        if (head == null)
+            return null;
 
-        while(l1 != null){
-            l2.next = new Node(l1.val);
-            mirror.put(l1, l2.next);
+        Node l1 = head;
+        // Create Cloned Nodes
+        while (l1 != null) {
+            Node l2 = new Node(l1.val);
+            l2.next = l1.next;
+            l1.next = l2;
+            l1 = l1.next.next;
+        }
+
+        l1 = head;
+        // Clone Random Pointers
+        while (l1 != null) {
+            if (l1.random != null)
+                l1.next.random = l1.random.next;
+            l1 = l1.next.next;
+        }
+
+        // Detach Two Lists
+        Node ans = head.next;
+        l1 = head;
+        Node l2 = head.next;
+        while (l1 != null) {
+            if (l1.next != null)
+                l1.next = l1.next.next;
+            if (l2.next != null)
+                l2.next = l2.next.next;
             l1 = l1.next;
             l2 = l2.next;
         }
 
-        l1 = head; l2 = dummy.next;
-        while(l1 != null){
-            l2.random = mirror.get(l1.random);
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-
-        return dummy.next;
-        
+        return ans;
     }
+
 }
