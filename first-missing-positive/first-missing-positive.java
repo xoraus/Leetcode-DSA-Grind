4e045f9ef39369deb9a916299fe7f3e7a84d6386
Sajjad Salaria <imsafay@gmail.com>
2023-05-19 23:32:51 +0530

    Time: 2 ms (54.38%) | Memory: 57.6 MB (5.84%) - LeetSync

--- first-missing-positive/first-missing-positive.java
+++ first-missing-positive/first-missing-positive.java
@@ -2,23 +2,23 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
         // Pass 1: Out of Range = Discard: Make them 0
-        for(int i=0; i<n; i++){
-            if(nums[i] <= 0 || nums[i] > n){
-                nums[i] = 0;
+        for(int idx = 0; idx < n; idx++){
+            if(nums[idx] <= 0 || nums[idx] > n){
+                nums[idx] = 0;
             }
         }
 
         // Pass 2: Mark Visited
-        for(int i=0; i<n; i++){
-            int val = nums[i] % (n+1);
+        for(int idx = 0; idx < n; idx++){
+            int val = nums[idx] % (n + 1);
             if(val > 0) nums[val-1] += (n+1);
         }
         // Pass 3: Missing +ve Integer
-        for(int i=0; i<n; i++){
-            int freq = nums[i] / (n+1);
-            if(freq == 0) return i+1;
+        for(int idx = 0; idx < n; idx++){
+            int freq = nums[idx] / (n + 1);
+            if(freq == 0) return idx + 1;
         }
 
-        return n+1; // corner case
+        return (n + 1); // corner case
     }
 }
