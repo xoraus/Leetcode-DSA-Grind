b16796bb007ad81c48d35c57f7ebe195dba3329a
Sajjad Salaria <imsafay@gmail.com>
2023-05-19 19:28:52 +0530

    Time: 14 ms (48.97%) | Memory: 57.6 MB (5.67%) - LeetSync

---
+++ valid-palindrome/valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s, int left, int right){
+        if(left >= right) return true;
+
+        char l = s.charAt(left);
+        char r = s.charAt(right);
+
+        if(Character.isLetterOrDigit(l) == false){
+            return isPalindrome(s, left + 1, right);
+        }
+
+        if(Character.isLetterOrDigit(r) == false){
+            return isPalindrome(s, left, right - 1);
+        }
+
+        if(l != r) return false;
+        return isPalindrome(s, left + 1, right - 1);
+    }
+    
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        return isPalindrome(s, 0, s.length() - 1);
+        
+    }
+}
