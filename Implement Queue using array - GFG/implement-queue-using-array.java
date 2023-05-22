9bebf0ba7c7f514a6081cacc4c2618d3c1ca72a5
Sajjad Salaria <imsafay@gmail.com>
2023-05-22 12:17:03 +0530

    Added solution - LeetHub

---
+++ Implement Queue using array - GFG/implement-queue-using-array.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+class GfG
+{
+	public static void main(String args[])
+	{
+		 Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+		 {
+			MyQueue obj = new MyQueue();
+			int Q = sc.nextInt();
+			while(Q>0)
+			{
+				int QueryType = 0;
+				QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					
+					obj.push(a);
+					
+				}else if(QueryType == 2)
+				{
+				System.out.print(obj.pop()+" ");
+				}
+				Q--;
+			}
+			System.out.println("");
+			t--;
+		 }
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+
+
+class MyQueue {
+
+    int front, rear, size = 0;
+	int arr[] = new int[100005];
+
+    MyQueue() {
+		front = 0;
+		rear = 0;
+	}
+	
+	//Function to push an element x in a queue.
+	void push(int value) {
+	    if(size == arr.length){
+	        return;
+	    }
+	    
+	    arr[rear] = value;
+	    rear++;
+	    size++;
+	} 
+
+    //Function to pop an element from queue and return that element.
+	int pop() {
+	    if(size == 0){
+	        return -1;
+	    }
+	    
+	    int ans = arr[front];
+	    front++;
+	    size--;
+
+	    return ans;
+	} 
+}
+
+
+
+
