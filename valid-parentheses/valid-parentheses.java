45fe86584a5efdc0fc6ebf13253d812c1598dd1e
Sajjad Salaria <imsafay@gmail.com>
2023-05-22 23:53:49 +0530

    Time: 4 ms (5.11%) | Memory: 40.6 MB (67.91%) - LeetSync

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,34 @@
+// Time = O(N), Space = O(N)
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        for (int idx = 0; idx < s.length(); idx++) {
+            char ch = s.charAt(idx);
+
+            if (ch == '}') {
+                // extra closing characters || type is mismatch
+                if (stk.size() == 0 || stk.peek() != '{')
+                    return false;
+                stk.pop();
+
+            } else if (ch == ']') {
+                if (stk.size() == 0 || stk.peek() != '[')
+                    return false;
+                stk.pop();
+
+            } else if (ch == ')') {
+                if (stk.size() == 0 || stk.peek() != '(')
+                    return false;
+                stk.pop();
+
+            } else
+                stk.push(ch);
+        }
+
+        if (stk.size() > 0)
+            return false; // extra opening characters
+        return true;
+    }
+}
