ec0b90a87e15936392fab737b43742a8a7ba4f62
Sajjad Salaria <imsafay@gmail.com>
2023-05-25 18:57:25 +0530

    Time: 32 ms (57.76%) | Memory: 44.8 MB (7.34%) - LeetSync

---
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        boolean[] unbalanced = new boolean[s.length()];
+
+        Stack<Integer> stk = new Stack<>();
+        for(int idx = 0; idx < s.length(); idx++){
+            if(s.charAt(idx) == '('){
+                stk.push(idx);
+                unbalanced[idx] = true;
+            } else if(s.charAt(idx) == ')'){
+                if(stk.size() > 0) {
+                    unbalanced[stk.pop()] = false;
+                } else{
+                    unbalanced[idx] = true;
+                }
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        for(int idx = 0; idx < s.length(); idx++){
+            if(unbalanced[idx] == false){
+                res.append(s.charAt(idx));
+            }
+        }
+
+        return res.toString();
+    }
+}
