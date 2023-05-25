263b19e3775336bc6e14ad7346d929455755505d
Sajjad Salaria <imsafay@gmail.com>
2023-05-25 17:52:33 +0530

    Time: 1 ms (100.00%) | Memory: 40.6 MB (72.55%) - LeetSync

---
+++ smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] freq = new int[26];
+        boolean[] visited = new boolean[26];
+
+        char[] chs = s.toCharArray();
+        for(char ch: chs) freq[ch - 'a']++;
+
+        StringBuilder res = new StringBuilder();
+
+        for(char ch: chs){
+            freq[ch - 'a']--;
+            if(visited[ch - 'a'] == true) continue;
+            visited[ch - 'a'] = true;
+
+            while(res.length() > 0){
+                char top = res.charAt(res.length() - 1);
+                if(top > ch && freq[top - 'a'] > 0){
+                    res.deleteCharAt(res.length() - 1);
+                    visited[top - 'a'] = false;
+                } else break;
+            }
+            res.append(ch);
+        }
+
+        return res.toString();
+    }
+}
