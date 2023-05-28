1fd34201e3632c312595fe8ddad04c71f4a3bb44
Sajjad Salaria <imsafay@gmail.com>
2023-05-28 12:57:51 +0530

    Time: 6 ms (77.83%) | Memory: 42.3 MB (90.99%) - LeetSync

--- reverse-words-in-a-string/reverse-words-in-a-string.java
+++ reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,12 +1,27 @@
 class Solution {
+    // Approach 2: 
+
     public String reverseWords(String str) {
-        String[] words = str.split("\s+");
-
         StringBuilder res = new StringBuilder();
-        for(int idx = words.length - 1; idx >= 0; idx--){
-            res.append(" " + words[idx]);
-        }
 
-        return res.toString().trim();
+        for(int idx = str.length() - 1; idx >= 0; idx--){
+            if(str.charAt(idx) != ' '){
+                StringBuilder word = new StringBuilder();
+                while(idx >= 0 && str.charAt(idx) != ' '){
+                    word.insert(0, str.charAt(idx));
+                    idx--;
+                }
+                if(res.length() == 0){
+                    res.append(word);
+                } else {
+                    res.append(" ");
+                    res.append(word);
+                }
+            }
+        }
+        
+        return res.toString();
     }
 }
+
+ 
