bec9331bfcca5f9969f4a45bf1ac9d1fd36d4266
Sajjad Salaria <imsafay@gmail.com>
2023-05-28 14:16:39 +0530

    Time: 4 ms (70.41%) | Memory: 41.9 MB (98.60%) - LeetSync

--- valid-palindrome/valid-palindrome.java
+++ valid-palindrome/valid-palindrome.java
@@ -1,25 +1,28 @@
+// Aprroach 2: Iterative
 class Solution {
-    public boolean isPalindrome(String s, int left, int right){
-        if(left >= right) return true;
+    public boolean isPalindrome(String str) {
+        str = str.toLowerCase();
+        int left = 0, right = str.length() - 1;
 
-        char l = s.charAt(left);
-        char r = s.charAt(right);
+        while (left <= right) {
+            if (!Character.isLetterOrDigit(str.charAt(left))) {
+                left++;
+                continue;
+            }
 
-        if(Character.isLetterOrDigit(l) == false){
-            return isPalindrome(s, left + 1, right);
+            if (!Character.isLetterOrDigit(str.charAt(right))) {
+                right--;
+                continue;
+            }
+
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
         }
-
-        if(Character.isLetterOrDigit(r) == false){
-            return isPalindrome(s, left, right - 1);
-        }
-
-        if(l != r) return false;
-        return isPalindrome(s, left + 1, right - 1);
-    }
-    
-    public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        return isPalindrome(s, 0, s.length() - 1);
         
+        return true;
     }
 }
