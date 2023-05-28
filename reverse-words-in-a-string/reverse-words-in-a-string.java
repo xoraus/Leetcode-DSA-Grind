ac9a590868894a93a6ede496e181744bd45ccb55
Sajjad Salaria <imsafay@gmail.com>
2023-05-28 13:13:44 +0530

    Time: 4 ms (92.97%) | Memory: 41.6 MB (99.52%) - LeetSync

--- reverse-words-in-a-string/reverse-words-in-a-string.java
+++ reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,25 +1,35 @@
 class Solution {
-    // Approach 2: 
+    // Approach 3: 
 
     public String reverseWords(String str) {
+        int right = str.length() - 1;
         StringBuilder res = new StringBuilder();
 
-        for(int idx = str.length() - 1; idx >= 0; idx--){
-            if(str.charAt(idx) != ' '){
-                StringBuilder word = new StringBuilder();
-                while(idx >= 0 && str.charAt(idx) != ' '){
-                    word.insert(0, str.charAt(idx));
-                    idx--;
-                }
-                if(res.length() == 0){
-                    res.append(word);
-                } else {
-                    res.append(" ");
-                    res.append(word);
-                }
+        // iterate on all words
+        while(right >= 0){
+
+            // right will stop at the ending index of a word.
+            while(right >= 0 && str.charAt(right) == ' '){
+                right--;
             }
+
+            int left = right;
+            // left will stop at the starting index of a word.
+            while(left >= 0 && str.charAt(left) != ' '){
+                left--;
+            }
+
+            if(res.length() > 0 && left < right){
+                res.append(" ");
+            }
+
+            for(int idx = left + 1; idx <= right; idx++){
+                res.append(str.charAt(idx));
+            }
+
+            right = left;
         }
-        
+
         return res.toString();
     }
 }
