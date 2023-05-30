c1594a37175be985880bcd3b6168689a74a9e960
Sajjad Salaria <imsafay@gmail.com>
2023-05-30 23:08:08 +0530

    Time: 63 ms (41.33%) | Memory: 42.1 MB (66.98%) - LeetSync

---
+++ call-function-with-custom-context/call-function-with-custom-context.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Object} context
+ * @param {any[]} args
+ * @return {any}
+ */
+
+// May 30 - Git Sync Test
+Object.defineProperty(Function.prototype, 'callPolyfill', {
+  value: function(obj, ...args) {
+    Object.defineProperty(obj, '__temp__', {
+      value: this,
+      enumerable: false
+    });
+
+    const result = obj.__temp__(...args);
+
+    delete obj.__temp__;
+
+    return result;
+  },
+  enumerable: false
+});
+
+
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
