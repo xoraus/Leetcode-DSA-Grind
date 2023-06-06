4fb4b6cd2bcde101dd9121b74976a911ca23accc
Sajjad Salaria <imsafay@gmail.com>
2023-06-06 18:29:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (89.49%) - LeetSync

--- flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,35 +1,42 @@
 class Solution {
-    public static class Pair{
-        TreeNode head, tail;
-        Pair(TreeNode head, TreeNode tail){
+    public static class Pair {
+        TreeNode head;
+        TreeNode tail;
+
+        Pair(TreeNode head, TreeNode tail) {
             this.head = head;
             this.tail = tail;
         }
     }
 
-    public Pair linearize(TreeNode root){
-        if(root == null){
+    public Pair linearize(TreeNode root) {
+        if (root == null) {
             return new Pair(null, null);
         }
 
-        if(root.left == null && root.right == null){
+        if (root.left == null && root.right == null) {
             return new Pair(root, root);
         }
 
-        if(root.left == null){
+        if (root.left == null) {
             Pair rightList = linearize(root.right);
             return new Pair(root, rightList.tail);
-        } 
+        }
+
+        if (root.right == null) {
+            Pair leftList = linearize(root.left);
+            root.right = leftList.head;
+            root.left = null; // Updated line
+            return new Pair(root, leftList.tail);
+        }
 
         Pair leftList = linearize(root.left);
         Pair rightList = linearize(root.right);
 
         root.left = null;
         root.right = leftList.head;
-        if(rightList.head == null){
-            return new Pair(root, leftList.tail);
-        }
         leftList.tail.right = rightList.head;
+
         return new Pair(root, rightList.tail);
     }
 
