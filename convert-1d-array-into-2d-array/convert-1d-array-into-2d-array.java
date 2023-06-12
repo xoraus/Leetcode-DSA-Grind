5304146a0d167ec4b4bf6cd5b5554b5e8aee29a4
Sajjad Salaria <imsafay@gmail.com>
2023-06-12 10:30:55 +0530

    Time: 6 ms (31.10%) | Memory: 55.4 MB (7.75%) - LeetSync

--- convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -1,16 +1,15 @@
 class Solution {
-    // Appraoch 1: 
-    
+    // Appraoch 2: 
+
     public int[][] construct2DArray(int[] original, int m, int n) {
         if(m * n != original.length) return new int[0][0];
 
         int[][] mat = new int[m][n];
 
-        int idx = 0;
-        for(int row = 0; row < m; row++){
-            for(int col = 0; col < n; col++){
-                mat[row][col] = original[idx++];
-            }
+        for(int idx = 0; idx < original.length; idx++){
+            int row = idx / n;
+            int col = idx % n;
+            mat[row][col] = original[idx];
         }
 
         return mat;
