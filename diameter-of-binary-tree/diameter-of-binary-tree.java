b1d7a3f26222d583b8b9eb95f2a93f953396a64d
Sajjad Salaria <imsafay@gmail.com>
2023-06-17 21:32:28 +0530

    Time: 1 ms (27.72%) | Memory: 43.1 MB (5.32%) - LeetSync

--- diameter-of-binary-tree/diameter-of-binary-tree.java
+++ diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     static class Pair {
-        int height = 0;
+        int height = -1;
         int diameter = 0;
     }
 
@@ -29,11 +29,11 @@ class Solution {
         Pair ans = new Pair();
         ans.height = Math.max(l.height, r.height) + 1;
         ans.diameter = Math.max(l.diameter, r.diameter);
-        ans.diameter = Math.max(ans.diameter,l.height + r.height + 1);
+        ans.diameter = Math.max(ans.diameter,l.height + r.height + 2);
         return ans;
     }
     public int diameterOfBinaryTree(TreeNode root) {
         Pair ans = dfs(root);
-        return ans.diameter - 1;
+        return ans.diameter;
     }
 }
