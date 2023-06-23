e0fee09d0a708ee1d0748d785a1919fae796088f
Sajjad Salaria <imsafay@gmail.com>
2023-06-23 12:00:13 +0530

    Time: 4 ms (42.60%) | Memory: 43.3 MB (34.06%) - LeetSync

---
+++ add-strings/add-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+        int p1 = num1.length() - 1, p2 = num2.length() - 1;
+        int carry = 0;
+
+        while(p1 >= 0 || p2 >= 0 || carry > 0){
+            int d1 = (p1 >= 0) ? num1.charAt(p1) - '0' : 0;
+            int d2 = (p2 >= 0) ? num2.charAt(p2) - '0' : 0;
+            int sum = d1 + d2 + carry;
+
+            carry = sum / 10;
+            res.insert(0, sum % 10);
+            p1--; p2--;
+        }
+
+        return res.toString();
+    }
+}
