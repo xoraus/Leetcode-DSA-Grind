cfec5f9c2764d0d6d759f22fce5084809875c455
Sajjad Salaria <imsafay@gmail.com>
2023-07-01 20:42:03 +0530

    Time: 0 ms (100.00%) | Memory: 39.4 MB (98.96%) - LeetSync

---
+++ fair-distribution-of-cookies/fair-distribution-of-cookies.java
@@ -0,0 +1,31 @@
+class Solution {
+    int ans;
+    int[] count;
+
+    public int distributeCookies(int[] cookies, int k) {
+        ans = Integer.MAX_VALUE;
+        count = new int[k];
+        backtrack(0, cookies, k);
+        return ans;
+    }
+
+    public void backtrack(int cookieNumber, int[] cookies, int k) {
+        if (cookieNumber == cookies.length) {
+            int max = 0;
+            for (int i = 0; i < k; i++) {
+                max = Math.max(max, count[i]);
+            }
+            ans = Math.min(ans, max);
+            return;
+        }
+
+        for (int i = 0; i < k; i++) {
+            count[i] += cookies[cookieNumber];
+            backtrack(cookieNumber + 1, cookies, k);
+            count[i] -= cookies[cookieNumber];
+            if (count[i] == 0) {
+                break;
+            }
+        }
+    }
+}
