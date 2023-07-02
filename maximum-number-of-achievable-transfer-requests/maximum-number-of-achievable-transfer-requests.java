9f4c3ad07091cd1e18edc28d8b146cd9c6ecc2c1
Sajjad Salaria <imsafay@gmail.com>
2023-07-02 12:42:20 +0530

    Time: 17 ms (90.91%) | Memory: 40.1 MB (96.97%) - LeetSync

--- maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.java
+++ maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.java
@@ -1,27 +1,29 @@
 class Solution {
-    int ans = 0;
-    int[][] requests;
-    public void backtracking(int[] transfers, int idx, int count){
-        if(idx == requests.length){
-            for(int req :transfers){
-                if(req != 0) return;
+    int max = 0;
+    public int maximumRequests(int n, int[][] requests) {
+        helper(requests, 0, new int[n], 0);
+        return max;
+    }
+    
+    private void helper(int[][] requests, int index, int[] count, int num) {
+        // Traverse all n buildings to see if they are all 0. (means balanced)
+        if (index == requests.length) {
+            for (int i : count) {
+                if (0 != i) {
+                    return;
+                }
             }
-            ans = Math.max(ans, count);
+            max = Math.max(max, num);
             return;
         }
-
-        transfers[requests[idx][0]]--;
-        transfers[requests[idx][1]]++;
-        backtracking(transfers, idx + 1, count + 1);
-        transfers[requests[idx][0]]++;
-        transfers[requests[idx][1]]--;
-        backtracking(transfers, idx + 1, count);
-
-    }
-    public int maximumRequests(int n, int[][] requests) {
-        this.requests = requests;
-        int[] transfers = new int[n];
-        backtracking(transfers, 0, 0);
-        return ans;
+		// Choose this request
+        count[requests[index][0]]++;
+        count[requests[index][1]]--;
+        helper(requests, index + 1, count, num + 1);
+        count[requests[index][0]]--;
+        count[requests[index][1]]++;
+        
+		// Not Choose the request
+        helper(requests, index + 1, count, num);
     }
 }
