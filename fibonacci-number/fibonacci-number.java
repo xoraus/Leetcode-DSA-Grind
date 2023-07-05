be4dae3a378317456bd813a8307aa1a10a05d89f
Sajjad Salaria <imsafay@gmail.com>
2023-07-05 12:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 39.4 MB (48.17%) - LeetSync

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,10 +1,17 @@
 class Solution {
-    public int fib(int n) {
+    public int helper(int n , int[] dp){
         if(n <= 1) return n;
+        if(dp[n] != -1) return dp[n];
 
-        int firstTerm = fib(n - 1);
-        int secondTerm = fib(n - 2);
+        int prev1 = helper(n - 1, dp);
+        int prev2 = helper(n - 2, dp);
+        dp[n] = prev1 + prev2;
 
-        return firstTerm + secondTerm;
+        return dp[n];
+    }
+    public int fib(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
     }
 }
