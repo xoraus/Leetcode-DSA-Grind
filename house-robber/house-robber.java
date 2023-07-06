20470048aa459ca107d2979138b8ece70c03b345
Sajjad Salaria <imsafay@gmail.com>
2023-07-06 20:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (32.22%) - LeetSync

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,21 +1,23 @@
 class Solution {
-    public int maxSum(int[] nums, int currIdx, int[] dp) {
-        if (currIdx >= nums.length)
-            return 0;
-        if (dp[currIdx] != -1)
-            return dp[currIdx];
-
-        dp[currIdx] = Math.max(nums[currIdx] + maxSum(nums, currIdx + 2, dp), maxSum(nums, currIdx + 1, dp));
-        return dp[currIdx];
-    }
-
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 0)
-            return 0;
+        if (n == 0) return 0;
 
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
         return maxSum(nums, 0, dp);
+    }
+
+    public int maxSum(int[] nums, int currIdx, int[] dp) {
+        if (currIdx >= nums.length) return 0;
+        
+        if (dp[currIdx] != -1)
+            return dp[currIdx];
+
+        int robCurrent = nums[currIdx] + maxSum(nums, currIdx + 2, dp);
+        int skipCurrent = maxSum(nums, currIdx + 1, dp);
+
+        dp[currIdx] = Math.max(robCurrent, skipCurrent);
+        return dp[currIdx];
     }
 }
