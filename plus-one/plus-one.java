3e05686408c75fa48a6fb1f671f004cb706c68be
Sajjad Salaria <imsafay@gmail.com>
2023-07-07 19:45:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (93.30%) - LeetSync

---
+++ plus-one/plus-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        
+        for(int idx = n - 1; idx >= 0; idx--){
+            if(digits[idx] == 9){
+                digits[idx] = 0;
+            } else {
+                digits[idx]++;
+                return digits;
+            }
+        }
+
+        int[] res = new int[n + 1];
+        res[0] = 1;
+
+        return res;
+    }
+}
