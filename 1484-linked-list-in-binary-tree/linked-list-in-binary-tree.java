e44d3150cbc3feb8c793963c8c9004515c50947d
Sajjad Salaria <imsafay@gmail.com>
2023-07-17 15:59:06 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (5.88%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,16 @@
+class Solution {
+    // fixing head node as the root node
+    public boolean dfs(ListNode head, TreeNode root){
+        if(head == null) return true;
+        if(root == null) return false;
+        if(head.val != root.val) return false;
+        return dfs(head.next, root.left) || dfs(head.next, root.right);
+    }
+
+    // we can start linked list from any node
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(head == null) return true;
+        if(root == null) return false;
+        return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+}
