6826fa7fec9c68ca27721e03a02bfc124dbc2d93
Sajjad Salaria <imsafay@gmail.com>
2023-07-24 15:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (84.40%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double power(double x, long n){
+        if(n == 0) return 1.0;
+
+        double smallWork = power(x, n / 2);
+        double bigWork = smallWork * smallWork;
+
+        if(n % 2 == 1) bigWork = bigWork * x;
+
+        return bigWork;
+    }
+    public double myPow(double x, int n) {
+        if(n < 0) return power(1 / x, -(long)n);
+        return power(x,n);
+    }
+}
