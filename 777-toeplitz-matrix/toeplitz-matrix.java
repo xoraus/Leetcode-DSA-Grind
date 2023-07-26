500c08a21f13392ce8ff09a502643f645aedb5d9
Sajjad Salaria <imsafay@gmail.com>
2023-07-26 11:29:50 +0530

    Time: 1 ms (75.08%) | Memory: 43.6 MB (27.36%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        for(int d = 1 - n; d <=  m - 1; d++){
+            int row = (d <= 0) ? 0 : d;
+            int col = (d <= 0) ? -d : 0;
+            int val = mat[row][col];
+
+            while(row < m && col < n){
+                if(mat[row][col] != val) return false;
+                row++; col++;
+            }
+        }
+        return true;
+    }
+}
