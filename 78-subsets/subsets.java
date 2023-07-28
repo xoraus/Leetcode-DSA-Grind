41b9647e4043cf9c2b2238962c9eebc800e5deb5
Sajjad Salaria <imsafay@gmail.com>
2023-07-28 20:04:01 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (64.23%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,17 +1,24 @@
 class Solution {
+
+    public void subsets(int[] nums, int idx, List<Integer> subset, List<List<Integer>> powerset){
+        if(idx == nums.length){
+            powerset.add(new ArrayList(subset));
+            return;
+        }
+
+        // yes
+        subset.add(nums[idx]);
+        subsets(nums, idx + 1, subset, powerset);
+        subset.remove(subset.size() - 1);
+
+        subsets(nums, idx + 1, subset, powerset);
+
+        return;
+    }
+
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> powerset = new ArrayList<>();
         subsets(nums, 0, new ArrayList<>(), powerset);
         return powerset;
     }
-
-    public void subsets(int[] nums, int idx, List<Integer> subset, List<List<Integer>> powerset) {
-        powerset.add(new ArrayList<>(subset));
-
-        for (int i = idx; i < nums.length; i++) {
-            subset.add(nums[i]);
-            subsets(nums, i + 1, subset, powerset);
-            subset.remove(subset.size() - 1);
-        }
-    }    
 }
