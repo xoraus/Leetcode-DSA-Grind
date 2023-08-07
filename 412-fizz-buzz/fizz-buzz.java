b8c7856eb74347abde75ddb221baa3a931903d33
Sajjad Salaria <imsafay@gmail.com>
2023-08-07 10:28:56 +0530

    Time: 1 ms (99.60%) | Memory: 45 MB (39.07%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> res = new ArrayList<>();
-        while(n > 0){
-            if(n % 3 == 0 && n % 5 == 0){
+        int currNum = 1;
+        
+        while(currNum <= n){
+            if(currNum % 3 == 0 && currNum % 5 == 0){
                 res.add("FizzBuzz");
-            } else if(n % 3 == 0){
+            } else if(currNum % 3 == 0){
                 res.add("Fizz");
-            } else if(n % 5 == 0){
+            } else if(currNum % 5 == 0){
                 res.add("Buzz");
             } else {
-                res.add(String.valueOf(n));
+                res.add(String.valueOf(currNum));
             }
-            n--;
+            currNum++;
         }
-        Collections.reverse(res);
         return res;
     }
 }
