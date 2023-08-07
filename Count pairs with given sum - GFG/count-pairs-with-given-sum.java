4e3a8832556c0246305e8c7b44f6e6c980926203
Sajjad Salaria <imsafay@gmail.com>
2023-08-07 11:52:00 +0530

    Added solution - LeetHub

--- Count pairs with given sum - GFG/count-pairs-with-given-sum.java
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.java
@@ -30,22 +30,49 @@ public class GFG {
 
 //User function Template for Java
 
-
-
-
 class Solution {
-    int getPairsCount(int[] nums, int length, int target) {
-        Map<Integer, Integer> complementFrequency = new HashMap<>();
-        int count = 0;
-        
-        for (int num : nums) {
-            int complement = target - num;
-            if (complementFrequency.containsKey(complement)) {
-                count += complementFrequency.get(complement);
+    int getPairsCount(int[] A, int n, int B) {
+    Arrays.sort(A);
+    int i = 0; // Left pointer
+    int j = A.length - 1; // Right pointer
+    long ans = 0; // To store the result (as it can be large, using long)
+    
+    while (i < j) {
+        if (A[i] + A[j] == B) {
+            int curIdxP1 = i; // Temporary pointer for A[i]
+            int currIdxP2 = j; // Temporary pointer for A[j]
+            
+            if (A[i] == A[j]) {
+                // Equal elements A[i] and A[j]
+                long cnt = j - i + 1; // Number of occurrences of A[i] and A[j]
+                ans += (cnt * (cnt - 1) / 2); // Counting pairs with equal elements
+                break;
+            } else {
+                // Different elements A[i] and A[j]
+                // Count number of elements with value A[i]
+                while (A[i] == A[curIdxP1]) {
+                    curIdxP1++;
+                }
+                int cnt1 = curIdxP1 - i; // Number of occurrences of A[i]
+                i = curIdxP1; // Move i to the next unique element
+                
+                // Count number of elements with value A[j]
+                while (A[currIdxP2] == A[j]) {
+                    currIdxP2--;
+                }
+                int cnt2 = j - currIdxP2; // Number of occurrences of A[j]
+                j = currIdxP2; // Move j to the next unique element
+                
+                ans += (cnt1 * cnt2); // Counting pairs with different elements
             }
-            complementFrequency.put(num, complementFrequency.getOrDefault(num, 0) + 1);
+        } else if (A[i] + A[j] > B) {
+            // Sum is greater than B, move j to the left to decrease the sum
+            j--;
+        } else {
+            // Sum is less than B, move i to the right to increase the sum
+            i++;
         }
-        
-        return count;
+    }
+    return (int) ans; // Convert the final result back to int and return
     }
 }
