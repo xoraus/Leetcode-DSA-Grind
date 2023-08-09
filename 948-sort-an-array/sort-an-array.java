5a1dd12a2c87aa712347a6b1a2665f8981bb8d41
Sajjad Salaria <imsafay@gmail.com>
2023-08-09 09:40:25 +0530

    Time: 5 ms (98.58%) | Memory: 56 MB (33.12%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,40 +1,26 @@
 class Solution {
-    public int[] mergeSort(int[] nums, int l, int r){
-        if(l==r){
-            return new int[]{nums[l]};
-        }
-        int mid = (l+r)/2;
-        int[] left = mergeSort(nums,l,mid);
-        int[] right = mergeSort(nums,mid+1,r);
-        return merge(left,right);
-    }
-    public int[] merge(int[] arr1, int[]arr2){
-        int[] res = new int[arr1.length+arr2.length];
-        int ptr1 = 0, ptr2 = 0, ptr3 = 0;
-        while(ptr1<arr1.length && ptr2 < arr2.length){
-            if(arr1[ptr1]<=arr2[ptr2]){
-                res[ptr3] = arr1[ptr1];
-                ptr1++;
-                ptr3++;
-            } else {
-                res[ptr3] = arr2[ptr2];
-                ptr2++;
-                ptr3++;
-            }
-        }
-        while(ptr1<arr1.length){
-            res[ptr3] = arr1[ptr1];
-            ptr1++;
-            ptr3++;
-        }
-        while(ptr2<arr2.length){
-            res[ptr3] = arr2[ptr2];
-            ptr2++;
-            ptr3++;
-        }
-        return res;
-    }
     public int[] sortArray(int[] nums) {
-        return mergeSort(nums, 0, nums.length-1);
+        int min = -50000, max = 50000;
+        int[] freq = new int[max - min + 1];
+
+        // Pass 1: Count the frequency of each element in the input array (O(N))
+        for (int ele : nums) {
+            freq[ele - min]++;
+        }
+
+        // Pass 2: Convert frequency array to prefix sum array (O(d))
+        for (int idx = 1; idx < freq.length; idx++) {
+            freq[idx] += freq[idx - 1];
+        }
+
+        // Pass 3: Create the output sorted array (O(N))
+        int[] res = new int[nums.length];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int ele = nums[i];
+            int last = freq[ele - min]--;
+            res[last - 1] = ele;
+        }
+
+        return res;
     }
 }
