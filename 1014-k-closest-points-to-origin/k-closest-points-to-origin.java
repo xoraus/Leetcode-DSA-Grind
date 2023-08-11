7ba6f6e7961757b81c5489aac12d8ad78feedc80
Sajjad Salaria <imsafay@gmail.com>
2023-08-11 12:33:32 +0530

    Time: 43 ms (34.12%) | Memory: 46.5 MB (94.51%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,20 +1,33 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int res[][] = new int[k][2];
-        
-        Arrays.sort(points,new Comparator<int[]>(){
-            public int compare(int[] a,int[] b){
-            int x = (a[0] * a[0]) + (a[1] * a[1]);
-            int y = (b[0] * b[0]) + (b[1] * b[1]);
-            return x - y;
-            }   
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> {
+            double distance1 = Math.sqrt(p1[0] * p1[0] + p1[1] * p1[1]);
+            double distance2 = Math.sqrt(p2[0] * p2[0] + p2[1] * p2[1]);
+            
+            // max-heap
+            if (distance1 < distance2) {
+                return +1;
+            } else if (distance2 < distance1) {
+                return -1;
+            } else {
+                return 0;
+            }
         });
 
-        for(int idx = 0; idx < k; idx++) {
-            res[idx][0] = points[idx][0];
-            res[idx][1] = points[idx][1];
+        for (int[] point : points) {
+            if (pq.size() < k) {
+                pq.add(point);
+            } else {
+                pq.add(point);
+                pq.remove();
+            }
         }
 
-        return res;
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.remove();
+        }
+
+        return result;
     }
 }
