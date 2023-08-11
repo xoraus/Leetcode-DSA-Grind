802f0d8d98602bdd218b5982caab4d42a048c403
Sajjad Salaria <imsafay@gmail.com>
2023-08-11 11:34:32 +0530

    Time: 24 ms (83.93%) | Memory: 54.3 MB (73.78%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,16 +1,37 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-        for (int num : nums) {
-            if (minHeap.size() < k) {
-                minHeap.add(num);
-            } else if (num > minHeap.peek()) {
-                minHeap.poll();
-                minHeap.add(num);
+    public int partition(int[] arr, int left, int right) {
+        int j = left;
+        for (int i = left; i < right; i++) {
+            if (arr[i] < arr[right]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
             }
         }
+        int temp = arr[j];
+        arr[j] = arr[right];
+        arr[right] = temp;
+        return j;
+    }
 
-        return minHeap.peek();
+    public int quickSelect(int[] arr, int left, int right, int k) {
+        if (left == right) {
+            return arr[left];
+        }
+        
+        int pivot = partition(arr, left, right);
+        
+        if (pivot == arr.length - k) {
+            return arr[pivot];
+        } else if (pivot > arr.length - k) {
+            return quickSelect(arr, left, pivot - 1, k);
+        } else {
+            return quickSelect(arr, pivot + 1, right, k);
+        }
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, k);
     }
 }
