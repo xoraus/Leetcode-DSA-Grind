3918172f5fd4f29b445295621a98039d27abeaea
Sajjad Salaria <imsafay@gmail.com>
2023-08-14 23:08:48 +0530

    Time: 25 ms (81.64%) | Memory: 54 MB (91.52%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,20 +1,8 @@
 class Solution {
-    public int partition(int[] arr, int left, int right) {
-        int j = left;
-        for (int i = left; i < right; i++) {
-            if (arr[i] < arr[right]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                j++;
-            }
-        }
-        int temp = arr[j];
-        arr[j] = arr[right];
-        arr[right] = temp;
-        return j;
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, k);
     }
-
+    
     public int quickSelect(int[] arr, int left, int right, int k) {
         if (left == right) {
             return arr[left];
@@ -30,8 +18,20 @@ class Solution {
             return quickSelect(arr, pivot + 1, right, k);
         }
     }
-
-    public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length - 1, k);
+    
+    public int partition(int[] arr, int left, int right) {
+        int j = left;
+        for (int i = left; i < right; i++) {
+            if (arr[i] < arr[right]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        int temp = arr[j];
+        arr[j] = arr[right];
+        arr[right] = temp;
+        return j;
     }
 }
