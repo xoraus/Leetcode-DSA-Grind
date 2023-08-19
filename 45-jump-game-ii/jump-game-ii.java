7ccebc17c0124b7dd7e4bffaf7de6c80c20c8b6b
Sajjad Salaria <imsafay@gmail.com>
2023-08-19 11:37:19 +0530

    Time: 1 ms (99.27%) | Memory: 44.2 MB (94.78%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,25 +1,18 @@
-class Solution {
+public class Solution {
     public int jump(int[] nums) {
-        int[] dp = new int[nums.length + 5];
-        Arrays.fill(dp, -1);
-        
-        int moves = memoization(0, nums.length - 1, nums, dp);
-        
-        return moves;
-    }
+        int n = nums.length;
+        int maxReachable = 0;
+        int currentEnd = 0;
+        int jumps = 0;
 
-    public int memoization(int src, int dest, int[] nums, int[] dp) {
-        if (src == dest) return 0;
-        if (src > dest) return (int) 1e7; // Infinity
-        if (dp[src] != -1) return dp[src];
-        
-        int ans = (int) 1e7; // Initialize with a large value
-        
-        for (int jump = 1; jump <= nums[src]; jump++) {
-            int moves = memoization(src + jump, dest, nums, dp);
-            ans = Math.min(ans, moves + 1);
+        for (int i = 0; i < n - 1; i++) {
+            maxReachable = Math.max(maxReachable, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = maxReachable;
+            }
         }
-        
-        return dp[src] = ans;
+
+        return jumps;
     }
 }
