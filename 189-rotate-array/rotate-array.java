dad3a7b9836d05e685d13233ba7ed430369bcd4d
Sajjad Salaria <imsafay@gmail.com>
2023-08-20 18:53:36 +0530

    Time: 1 ms (49.47%) | Memory: 55.4 MB (75.55%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,20 +1,30 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int size = nums.length;
-        k = k % size;
+        int n = nums.length;
 
-        reverse(nums, 0, size - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, size - 1);
-    }
+        if (n == 0) return;
 
-    public static void reverse(int[] nums, int startIdx, int endIdx) {
-        while (startIdx < endIdx) {
-            int temp = nums[startIdx];
-            nums[startIdx] = nums[endIdx];
-            nums[endIdx] = temp;
-            startIdx++;
-            endIdx--;
+        // Get the effective number of rotations:
+        k = k % n;
+
+        // If k is 0, the array remains unchanged
+        if (k == 0) return;
+
+        int[] temp = new int[k]; // temporary array
+
+        // Step 1: Copy the last k elements to the temporary array
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
+        }
+
+        // Step 2: Shift the first (n - k) elements to the right by k places
+        for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+
+        // Step 3: Place the elements of the temporary array at the beginning of the array
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
         }
     }
 }
