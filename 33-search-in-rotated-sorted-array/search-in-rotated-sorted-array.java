54533f5db2dc204318728b4adb21f4a1924ac9e0
Sajjad Salaria <imsafay@gmail.com>
2023-08-25 11:20:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (13.75%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,44 +1,30 @@
 class Solution {
-    public int findMin(int[] nums) {
-    int n = nums.length;
-    if (n == 1 || nums[0] < nums[n - 1])
-        return 0;
-    // Array not rotated at all: completely sorted
-
-    int left = 1, right = n - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid - 1] > nums[mid])
-            return mid;
-        if (nums[mid] >= nums[right])
-            left = mid + 1; // rotated part: min in right side
-        else
-            right = mid - 1; // unrotated part: min in left side
-    }
-
-    return -1;
-}
-
-    public int binarySearch(int[] nums, int left, int right, int target) {
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-
-        return -1;
-    }
-
     public int search(int[] nums, int target) {
-        int pi = findMin(nums);
-        int lans = binarySearch(nums, 0, pi - 1, target);
-        if (lans != -1)
-            return lans;
+        int low = 0, high = nums.length - 1;
 
-        return binarySearch(nums, pi, nums.length - 1, target);
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            // if mid is the target
+            if(nums[mid] == target) return mid;
+
+            // if left half part is sorted
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    // element exists
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else { // if right half part is sorted
+                if(nums[mid] <= target && target <= nums[high]){
+                    // element exists
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 }
