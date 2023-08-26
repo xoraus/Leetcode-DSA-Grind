a4f3908d167b771cdaa7875161f70e714e90b973
Sajjad Salaria <imsafay@gmail.com>
2023-08-26 13:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (90.00%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,30 +1,5 @@
 class Solution {
-    public boolean rotateString(String original, String target) {
-        if (original == null || target == null) {
-            // Throw an exception on both original and target being null?
-            return false;
-        }
-        if (original.length() != target.length()) {
-            return false;
-        }
-        if (original.length() == 0) {
-            return true;
-        }
-        for (int rotation = 0; rotation < original.length(); rotation++) {
-            if (isRotatedString(original, target, rotation)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    private boolean isRotatedString(String original, String target, int rotation) {
-        for (int i = 0; i < original.length(); i++) {
-            int rotatedIndex = (i + rotation) % target.length();
-            if (original.charAt(i) != target.charAt(rotatedIndex)) {
-                return false;
-            }
-        }
-        return true;
+    public boolean rotateString(String A, String B) {
+        return A.length() == B.length() && (A + A).contains(B);
     }
 }
