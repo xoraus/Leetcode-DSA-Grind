e291eff6ebae162f66249002af1d1943467ea1c2
Sajjad Salaria <imsafay@gmail.com>
2023-08-27 12:01:21 +0530

    Time: 1 ms (98.30%) | Memory: 43.3 MB (45.32%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,30 +1,27 @@
 class Solution {
     public int compress(char[] chars) {
-        String str = new String(chars);
-        StringBuilder res = new StringBuilder();
+        int resIndex = 0;
         int left = 0, right = 0;
         
-        while (left < str.length()) {
-            char ch = str.charAt(left);
+        while (left < chars.length) {
+            char ch = chars[left];
             int freq = 0;
             
-            while (right < str.length() && str.charAt(right) == ch) {
+            while (right < chars.length && chars[right] == ch) {
                 freq++;
                 right++;
             }
             
-            res.append(ch);
+            chars[resIndex++] = ch;
             if (freq > 1) {
-                res.append(freq);
+                String freqStr = Integer.toString(freq);
+                for (char digit : freqStr.toCharArray()) {
+                    chars[resIndex++] = digit;
+                }
             }
             left = right;
         }
-        
-        String compressed = res.toString();
-        for (int i = 0; i < compressed.length(); i++) {
-            chars[i] = compressed.charAt(i);
-        }
 
-        return compressed.length();
+        return resIndex;
     }
 }
