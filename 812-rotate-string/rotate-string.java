e8c01e81a990e56f04a53a631a8fb6955629feda
Sajjad Salaria <imsafay@gmail.com>
2023-09-29 11:29:33 +0530

    Time: 1 ms (29.66%) | Memory: 40.5 MB (48.58%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,5 +1,48 @@
 class Solution {
-    public boolean rotateString(String A, String B) {
-        return A.length() == B.length() && (A + A).contains(B);
+    public boolean rotateString(String s, String goal) {
+        int n = s.length();
+        int m = goal.length();
+        
+        // Check if lengths are equal; if not, return false
+        if (n != m) {
+            return false;
+        }
+        
+        // Concatenate s with itself to form a doubled string
+        String doubledString = s + s;
+        
+        // Build the prefix function for goal
+        int[] prefixFunction = new int[m];
+        int j = 0; // The length of the previous longest prefix suffix
+        
+        for (int i = 1; i < m; i++) {
+            while (j > 0 && goal.charAt(i) != goal.charAt(j)) {
+                j = prefixFunction[j - 1];
+            }
+            
+            if (goal.charAt(i) == goal.charAt(j)) {
+                j++;
+            }
+            
+            prefixFunction[i] = j;
+        }
+        
+        // Traverse the concatenated string
+        j = 0; // Pointer for goal
+        for (int i = 0; i < 2 * n; i++) {
+            while (j > 0 && doubledString.charAt(i) != goal.charAt(j)) {
+                j = prefixFunction[j - 1];
+            }
+            
+            if (doubledString.charAt(i) == goal.charAt(j)) {
+                j++;
+            }
+            
+            if (j == m) {
+                return true; // Found a match
+            }
+        }
+        
+        return false; // No match found
     }
 }
