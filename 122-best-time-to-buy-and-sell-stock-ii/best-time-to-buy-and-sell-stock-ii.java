ad81450b9a81eae8e4fecccde27d153c7d7384a8
Sajjad Salaria <imsafay@gmail.com>
2023-11-01 22:20:34 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (84.63%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        Stack<Integer> stack = new Stack<>();
+        int buyingPrice = prices[0];
         int profit = 0;
 
         for (int i = 0; i < prices.length; i++) {
-            if (!stack.isEmpty() && stack.peek() < prices[i]) {
-                profit += prices[i] - stack.pop();
+            if (buyingPrice < prices[i]) {
+                profit += prices[i] - buyingPrice;
             }
-            stack.push(prices[i]);
+            buyingPrice = prices[i];
         }
 
         return profit;
