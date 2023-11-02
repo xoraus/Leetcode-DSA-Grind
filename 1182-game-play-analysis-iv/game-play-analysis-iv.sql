04e0564f751a0d7a7ca79b78889ebdbbc8199422
Sajjad Salaria <imsafay@gmail.com>
2023-11-02 15:07:13 +0530

    Time: 1029 ms (50.50%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,21 @@
+WITH first_logins AS (
+  SELECT
+    A.player_id,
+    MIN(A.event_date) AS first_login
+  FROM
+    Activity A
+  GROUP BY
+    A.player_id
+), consec_logins AS (
+  SELECT
+    COUNT(A.player_id) AS num_logins
+  FROM
+    first_logins F
+    INNER JOIN Activity A ON F.player_id = A.player_id
+    AND F.first_login = DATE_SUB(A.event_date, INTERVAL 1 DAY)
+)
+SELECT
+  ROUND(
+    (SELECT C.num_logins FROM consec_logins C)
+    / (SELECT COUNT(F.player_id) FROM first_logins F)
+  , 2) AS fraction;
