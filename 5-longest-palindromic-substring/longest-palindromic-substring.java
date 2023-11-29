7d602ff1d7555603bb56105c4161ff6ce55304d3
Sajjad Salaria <imsafay@gmail.com>
2023-11-29 13:08:26 +0530

    Time: 19 ms (59.37%) | Memory: 44.5 MB (24.24%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,46 +1,28 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        StringBuilder rev = new StringBuilder(s);
-        rev.reverse();
-        String s2 = new String(rev);
-        return longestCommonSubstring(s, s2);
-    }
+    public String longestPalindrome(String str) {
+        if (str.isEmpty()) return "";
 
-    public String longestCommonSubstring(String s1, String s2) {
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        String longestPalindrome = "";
+        for (int i = 0; i < str.length(); i++) {
+            String palindrome1 = expandAroundCenter(str, i, i);
+            String palindrome2 = expandAroundCenter(str, i, i + 1);
 
-        String res = "";
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            for (int j = s2.length() - 1; j >= 0; j--) {
-                char ch1 = s1.charAt(i);
-                char ch2 = s2.charAt(j);
-
-                if (ch1 == ch2) {
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-
-                    if (dp[i][j] >= res.length()) {
-                        String curr = s1.substring(i, i + dp[i][j]);
-                            if (isPalindrome(curr)) {
-                            res = curr;
-                        }
-                    }
-                }
+            if (palindrome1.length() > longestPalindrome.length()) {
+                longestPalindrome = palindrome1;
+            }
+            if (palindrome2.length() > longestPalindrome.length()) {
+                longestPalindrome = palindrome2;
             }
         }
-        return res;
+        return longestPalindrome;
     }
 
-    public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+    private String expandAroundCenter(String str, int left, int right) {
+        while (left >= 0 && right < str.length() 
+            && str.charAt(left) == str.charAt(right)) {
+            left--;
+            right++;
         }
-        return true;
+        return str.substring(left + 1, right);
     }
 }
