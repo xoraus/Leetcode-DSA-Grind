28f98935ecdd31a10ac2f6a22eb6f935240dea79
Sajjad Salaria <imsafay@gmail.com>
2023-11-29 13:07:27 +0530

    Time: 1680 ms (5.03%) | Memory: 56.4 MB (5.12%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,32 +1,46 @@
 class Solution {
-    public String longestPalindrome(String str) {
-        int length = str.length();
-        if (length == 0) {
-            return "";
-        }
-        
-        // A single character itself is a palindrome
-        String longestPalindrome = str.substring(0, 1); 
-        for (int i = 0; i < length - 1; i++) {
-            String palindrome1 = expandAroundCenter(str, i, i);
-            if (palindrome1.length() > longestPalindrome.length()) {
-                longestPalindrome = palindrome1;
-            }
-            String palindrome2 = expandAroundCenter(str, i, i + 1);
-            if (palindrome2.length() > longestPalindrome.length()) {
-                longestPalindrome = palindrome2;
-            }
-        }
-        return longestPalindrome;
+    public String longestPalindrome(String s) {
+        StringBuilder rev = new StringBuilder(s);
+        rev.reverse();
+        String s2 = new String(rev);
+        return longestCommonSubstring(s, s2);
     }
 
-    public String expandAroundCenter(String str, int center1, int center2) {
-        int left = center1, right = center2;
-        int length = str.length();
-        while (left >= 0 && right <= length - 1 && str.charAt(left) == str.charAt(right)) {
-            left--;
-            right++;
+    public String longestCommonSubstring(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        String res = "";
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            for (int j = s2.length() - 1; j >= 0; j--) {
+                char ch1 = s1.charAt(i);
+                char ch2 = s2.charAt(j);
+
+                if (ch1 == ch2) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+
+                    if (dp[i][j] >= res.length()) {
+                        String curr = s1.substring(i, i + dp[i][j]);
+                            if (isPalindrome(curr)) {
+                            res = curr;
+                        }
+                    }
+                }
+            }
         }
-        return str.substring(left + 1, right);
+        return res;
+    }
+
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
