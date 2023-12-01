1a4ac68dfa47c8c3d5293c90768a383e7ed804e0
Sajjad Salaria <imsafay@gmail.com>
2023-12-01 23:24:24 +0530

    Time: 3 ms (7.87%) | Memory: 40.3 MB (54.27%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,16 +1,33 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        
-        for (String word : word1) {
-            sb1.append(word);
+        int lengthWord1 = word1.length;
+        int lengthWord2 = word2.length;
+
+        int indexWord1 = 0, charIndexWord1 = 0;
+        int indexWord2 = 0, charIndexWord2 = 0;
+
+        while (indexWord1 < lengthWord1 && indexWord2 < lengthWord2) {
+            char currentCharWord1 = word1[indexWord1].charAt(charIndexWord1);
+            char currentCharWord2 = word2[indexWord2].charAt(charIndexWord2);
+
+            if (currentCharWord1 != currentCharWord2)
+                return false;
+
+            charIndexWord1++;
+            charIndexWord2++;
+
+            if (charIndexWord1 == word1[indexWord1].length()) {
+                charIndexWord1 = 0;
+                indexWord1++;
+            }
+
+            if (charIndexWord2 == word2[indexWord2].length()) {
+                charIndexWord2 = 0;
+                indexWord2++;
+            }
         }
 
-        for (String word : word2) {
-            sb2.append(word);
-        }
-
-        return sb1.toString().equals(sb2.toString());
+        return indexWord1 == lengthWord1 && indexWord2 == lengthWord2;
     }
+
 }
