ef47aea8fed2fff93e8f5a0a921c0167a2354585
Sajjad Salaria <imsafay@gmail.com>
2023-12-04 16:57:21 +0530

    Time: 6 ms (59.70%) | Memory: 43.6 MB (34.29%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,6 +1,17 @@
 class Solution {
     HashSet<String> dict;
 
+    public boolean wordBreak(String s, List<String> wordDict) {
+        dict = new HashSet<>();
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp, -1);
+
+        for (String str : wordDict)
+            dict.add(str);
+
+        return (wordBreak(0, s, dp) == 1) ? true : false;
+    }
+
     public int wordBreak(int i, String s, int[] dp) {
         if (i == s.length())
             return 1;
@@ -14,16 +25,5 @@ class Solution {
         }
 
         return dp[i] = 0;
-    }
-
-    public boolean wordBreak(String s, List<String> wordDict) {
-        dict = new HashSet<>();
-        int[] dp = new int[s.length() + 1];
-        Arrays.fill(dp, -1);
-
-        for (String str : wordDict)
-            dict.add(str);
-
-        return (wordBreak(0, s, dp) == 1) ? true : false;
-    }
+    } 
 }
