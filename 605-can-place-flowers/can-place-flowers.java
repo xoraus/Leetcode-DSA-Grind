cff84199f85e08037d2c5394314241de24c948de
Sajjad Salaria <imsafay@gmail.com>
2023-12-08 01:46:42 +0530

    Time: 1 ms (98.29%) | Memory: 44.6 MB (17.99%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,15 +1,22 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int max = 0, firstIndex = -2, lastIndex = -1;
-
+        int max = 0, firstIndex = -1, lastIndex = -1, count = 0;
         for (int i = 0; i < flowerbed.length; i++) {
             if (flowerbed[i] == 1) {
-                max += (i - firstIndex - 2) / 2;
-                firstIndex = i;
+                if (firstIndex == -1) firstIndex = i;
+                lastIndex = i;
             }
         }
-
-        max += (flowerbed.length - firstIndex - 1) / 2;
+        if (firstIndex == -1) return (flowerbed.length + 1) / 2 >= n;
+        max += firstIndex / 2 + (flowerbed.length - lastIndex - 1) / 2;
+        for (int i = firstIndex + 1; i <= lastIndex - 1; i++) {
+            if (flowerbed[i] == 0) count++;
+            else {
+                max += (count - 1) / 2;
+                count = 0;
+            }
+        }
+        max += (count - 1) / 2;
         return max >= n;
     }
 }
