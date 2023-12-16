f7fe9b03aace4c63d2d8ea5a8034380ca43249e6
Sajjad Salaria <imsafay@gmail.com>
2023-12-16 12:13:59 +0530

    Time: 2 ms (98.39%) | Memory: 43.2 MB (66.13%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] getFrequency(String str){
+        int[] freq = new int[26];
+        
+        for(char ch: str.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        return freq;
+    }
+
+    public boolean isAnagram(String s1, String s2) {
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        
+        int[] f1 = getFrequency(s1);
+        int[] f2 = getFrequency(s2);
+
+        for(int idx = 0; idx < 26; idx++){
+            if(f1[idx] != f2[idx])
+                return false;
+        }
+
+        return true;
+    }
+}
