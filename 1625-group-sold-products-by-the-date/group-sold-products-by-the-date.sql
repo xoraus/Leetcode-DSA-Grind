48e722014eac700111dd7601b4965b3bf27ec567
Sajjad Salaria <imsafay@gmail.com>
2023-12-20 16:58:12 +0530

    Time: 1979 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,7 @@
+SELECT  
+	sell_date,
+	(COUNT(sell_date )) as num_sold ,
+	GROUP_CONCAT(distinct product  ORDER BY product) as products 
+FROM 
+	(SELECT DISTINCT sell_date,product FROM Activities) as Activities
+GROUP BY sell_date;
