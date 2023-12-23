18851ec0463114b61a3c344f51adc75cd5d0c7ee
Sajjad Salaria <imsafay@gmail.com>
2023-12-23 21:19:28 +0530

    Time: 6 ms (14.98%) | Memory: 42.2 MB (6.17%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        Set<String> set = new HashSet<>();
+
+        int x = 0;
+        int y = 0;
+        String key = x + "_" + y;
+        set.add(key);
+
+        for (char ch : path.toCharArray()) {
+            if (ch == 'E') {
+                x += 1;
+            } else if (ch == 'W') {
+                x -= 1;
+            } else if (ch == 'N') {
+                y += 1;
+            } else {
+                y -= 1;
+            }
+
+            key = x + "_" + y;
+            if (set.contains(key)) {
+                return true;
+            }
+
+            set.add(key);
+        }
+
+        return false;
+    }
+}
