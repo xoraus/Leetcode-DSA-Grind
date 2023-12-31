c1343ab5e0e1ed45aa40a46dcea55c232d13128d
Sajjad Salaria <imsafay@gmail.com>
2023-12-31 16:58:26 +0530

    Time: 1 ms (86.22%) | Memory: 41.5 MB (11.53%) - LeetSync

---
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int[] v1 = new int[26];
+        int[] v2 = new int[26];
+        Arrays.fill(v1, -1);
+        Arrays.fill(v2, -1);
+        int ans = -1;
+
+        for (int i = 0; i < s.length(); ++i) {
+            int temp = s.charAt(i) - 'a';
+
+            if (v1[temp] == -1) {
+                v1[temp] = i;
+            } else {
+                v2[temp] = i;
+                ans = Math.max(ans, v2[temp] - v1[temp] - 1);
+            }
+        }
+
+        return ans;
+    }
+}
