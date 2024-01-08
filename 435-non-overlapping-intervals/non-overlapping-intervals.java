65ef76d2139c53283718ce9057a5a6745de921e1
Sajjad Salaria <imsafay@gmail.com>
2024-01-08 17:24:05 +0530

    Time: 53 ms (33.37%) | Memory: 91.5 MB (60.40%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,17 +1,30 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (t, o) -> Integer.compare(o[0], t[0]));
+        // Sort intervals based on the starting points
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
 
-        int removals = 0, nextEnd = Integer.MAX_VALUE;
+        int removals = 0;
+        
+        if (intervals.length > 0) {
+            // Initialize nextEnd with the ending point of the first interval
+            int nextEnd = intervals[0][1];
 
-        for (int[] curr : intervals) {
-            if (curr[1] <= nextEnd) {
-                nextEnd = curr[0];
-            } else {
-                removals++;
+            // Iterate through intervals to find minimum removals
+            for (int i = 1; i < intervals.length; i++) {
+                int[] curr = intervals[i];
+                if (curr[0] < nextEnd) {
+                    // Overlapping interval found, increment removals
+                    removals++;
+                    // Update nextEnd to the minimum ending point
+                    nextEnd = Math.min(nextEnd, curr[1]);
+                } else {
+                    // Update nextEnd with the ending point of the current interval
+                    nextEnd = curr[1];
+                }
             }
         }
 
+        // Return the minimum number of removals
         return removals;
     }
 }
