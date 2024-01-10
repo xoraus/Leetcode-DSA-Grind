328821db8cef9877bb4c0ce1be3078fa73095dd4
Sajjad Salaria <imsafay@gmail.com>
2024-01-10 15:32:51 +0530

    Time: 1 ms (79.42%) | Memory: 41.4 MB (5.51%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,7 +1,17 @@
 class Solution {
     public int addDigits(int num) {
-        if(num == 0) return 0;
-        else if(num % 9 == 0) return 9;
-        else return num % 9;
+        while (num >= 10) {
+            num = sumOfDigits(num);
+        }
+        return num;
+    }
+
+    public static int sumOfDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
 }
