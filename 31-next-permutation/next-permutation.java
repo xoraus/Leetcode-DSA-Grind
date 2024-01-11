7c06249cb4f2570d01e3c334649273336086dfb2
Sajjad Salaria <imsafay@gmail.com>
2024-01-11 12:48:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (18.41%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,44 +1,45 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int breakPoint = findBreakPoint(nums, n);
-
-        if (breakPoint == -1) {
-            reverse(nums, 0, n - 1);
-        } else {
-            int nextGreaterIndex = findNextGreater(nums, breakPoint, n);
-            swap(nums, breakPoint, nextGreaterIndex);
-            reverse(nums, breakPoint + 1, n - 1);
+    public void nextPermutation(int[] numbers) {
+        int arrayLength = numbers.length;
+        
+        int pivotIndex = findPivot(numbers, arrayLength);
+        if(pivotIndex != 0) {
+            int swapIndex = findSwapIndex(numbers, arrayLength, pivotIndex);
+            swapElements(numbers, pivotIndex - 1, swapIndex);
         }
+        reverseSubarray(numbers, pivotIndex, arrayLength - 1);
     }
 
-    private int findBreakPoint(int[] nums, int n) {
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                return i;
+    private int findPivot(int[] numbers, int arrayLength) {
+        int pivotIndex = arrayLength - 1;
+        for(; pivotIndex > 0; pivotIndex--) {
+            if(numbers[pivotIndex] > numbers[pivotIndex - 1]) {
+                break;
             }
         }
-        return -1;
+        return pivotIndex;
     }
 
-    private int findNextGreater(int[] nums, int breakPoint, int n) {
-        for (int i = n - 1; i > breakPoint; i--) {
-            if (nums[i] > nums[breakPoint]) {
-                return i;
+    private int findSwapIndex(int[] numbers, int arrayLength, int pivotIndex) {
+        int swapIndex = pivotIndex;
+        for(int currentIndex = arrayLength - 1; currentIndex >= pivotIndex; currentIndex--) {
+            if(numbers[currentIndex] > numbers[pivotIndex - 1]) {
+                swapIndex = currentIndex;
+                break;
             }
         }
-        return -1;
+        return swapIndex;
     }
 
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    private void swapElements(int[] numbers, int index1, int index2) {
+        int temp = numbers[index1];
+        numbers[index1] = numbers[index2];
+        numbers[index2] = temp;
     }
 
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
+    private void reverseSubarray(int[] numbers, int start, int end) {
+        while(start < end) {
+            swapElements(numbers, start, end);
             start++;
             end--;
         }
