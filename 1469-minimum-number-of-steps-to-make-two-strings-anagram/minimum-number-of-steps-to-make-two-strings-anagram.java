ea52bea38dfd1b261668960710159f5eae250bbd
Sajjad Salaria <imsafay@gmail.com>
2024-01-13 15:41:49 +0530

    Time: 8 ms (76.51%) | Memory: 45.5 MB (11.92%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,20 +1,19 @@
-class Solution {
+public class Solution {
     public int minSteps(String s, String t) {
         int n = s.length();
         
         int minSteps = 0;
-        int[] freqMap1 = new int[26];
-        int[] freqMap2 = new int[26];
+        int[] map = new int[26];
 
-        for (int idx = 0; idx < n; idx++) {
-            freqMap1[s.charAt(idx) - 'a']++;
-            freqMap2[t.charAt(idx) - 'a']++;
+        for (int i = 0; i < n; i++) {
+            map[s.charAt(i) - 'a']++;
+            map[t.charAt(i) - 'a']--;
+        }        
+
+        for (int i = 0; i < 26; i++) {
+            minSteps += Math.max(0, map[i]);
         }
 
-        for (int idx = 0; idx < 26; idx++) {
-            minSteps += Math.abs(freqMap1[idx] - freqMap2[idx]);
-        }
-
-        return minSteps / 2; 
+        return minSteps;
     }
 }
