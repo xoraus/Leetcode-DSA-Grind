844a1fbe88f4e70b90d7df102505cd2959e589bc
Sajjad Salaria <imsafay@gmail.com>
2024-01-15 17:04:03 +0530

    Time: 85 ms (52.81%) | Memory: 114.3 MB (5.15%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -1,30 +1,33 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> res0 = new ArrayList<>();
-        List<Integer> res1 = new ArrayList<>();
+        List<Integer> notLost = new ArrayList<>();
+        List<Integer> oneLost = new ArrayList<>();
 
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
-        for(int[] match: matches){
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        for (int[] match : matches) {
             map1.put(match[0], map1.getOrDefault(match[0], 0) + 1);
             map2.put(match[1], map2.getOrDefault(match[1], 0) + 1);
         }
 
-        for(int key: map2.keySet()){
-            if(map2.get(key) == 1) res1.add(key);
+        for (int key : map2.keySet()) {
+            if (map2.get(key) == 1) {
+                oneLost.add(key);
+            }
         }
 
-        for(int key: map1.keySet()){
-            if(map2.get(key) == null) res0.add(key);
+        for (int key : map1.keySet()) {
+            if (map2.get(key) == null) {
+                notLost.add(key);
+            }
         }
 
-        Collections.sort(res0);
-        Collections.sort(res1);
+        Collections.sort(notLost);
+        Collections.sort(oneLost);
 
-        res.add(res0);
-        res.add(res1);
-
-        return res;
+        return Arrays.asList(notLost, oneLost);
     }
 }
