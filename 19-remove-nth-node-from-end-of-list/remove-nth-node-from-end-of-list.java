d0d7d362997176173e8cd5c10ad5ba2638466986
Sajjad Salaria <imsafay@gmail.com>
2024-02-03 15:39:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (60.61%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -3,40 +3,37 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null || n <= 0) {
-            return head; // No modification needed for invalid input.
-        }
+    public ListNode removeNthFromEnd(ListNode head, int k) {
+        ListNode slow = head, fast = head, prev = null;
 
-        ListNode dummy = new ListNode(0); // Create a dummy node to simplify edge cases.
-        dummy.next = head;
-
-        ListNode slow = dummy;
-        ListNode fast = dummy;
-
-        // Move the fast pointer B+1 steps ahead.
-        for (int i = 0; i <= n; i++) {
-            if (fast == null) {
-                // B is greater than or equal to the size of the list.
-                // Remove the first node by updating the head.
-                return head.next;
-            }
+        // Fast should be k dist ahead of slow
+        for (int idx = 0; idx < k; idx++) {
+            if (fast == null) return head;
             fast = fast.next;
         }
 
-        // Move the slow and fast pointers together until the fast reaches the end.
+        // Handle the case where the node to be removed is the head
+        if (fast == null) {
+            return head.next;
+        }
+
+        // Sliding Window
         while (fast != null) {
+            prev = slow;
             slow = slow.next;
             fast = fast.next;
         }
 
-        // Remove the B-th node from the end by adjusting the pointers.
-        slow.next = slow.next.next;
+        // Remove the Nth node
+        prev.next = slow.next;
 
-        return dummy.next; // Return the modified head of the list.
+        return head;
     }
+
 }
