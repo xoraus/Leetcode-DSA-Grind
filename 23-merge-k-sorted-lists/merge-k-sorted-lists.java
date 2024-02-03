cf421e6e449d3e70587ce2a38aa7dd4c358fd109
Sajjad Salaria <imsafay@gmail.com>
2024-02-03 16:18:02 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (46.34%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,14 +9,6 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        ListNode res = null;
-        for(ListNode head: lists){
-            res = merge(res, head);
-        }
-        return res; 
-    }
-
     public ListNode merge(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
         ListNode tail = dummy;
@@ -38,5 +30,20 @@ class Solution {
         else
             tail.next = list2;
         return dummy.next;
+    }
+
+    public ListNode helper(ListNode[] arr, int left, int right) {
+        if (left == right)
+            return arr[left];
+        int mid = (left + right) / 2;
+        ListNode l1 = helper(arr, left, mid);
+        ListNode l2 = helper(arr, mid + 1, right);
+        return merge(l1, l2);
+    }
+
+    public ListNode mergeKLists(ListNode[] arr) {
+        if (arr.length == 0)
+            return null;
+        return helper(arr, 0, arr.length - 1);
     }
 }
