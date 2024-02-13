804975fc93c0f854251f3389eba6bcbf87621015
Sajjad Salaria <imsafay@gmail.com>
2024-02-13 12:26:11 +0530

    Time: 72 ms (12.37%) | Memory: 50.5 MB (49.76%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int sumSubarrayMins(int[] nums) {
+        int[] left = leftNSE(nums);
+        int[] right = rightNSE(nums);
+        long ans = 0;
+
+        for (int curr = 0; curr < nums.length; curr++) {
+            long count = (right[curr] - curr) * (curr - left[curr]);
+            ans = (ans + (count * nums[curr]) % 1000000007) % 1000000007;
+        }
+
+        return (int) ans;
+    }
+
+    public int[] rightNSE(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stk = new Stack<>();
+        int[] res = new int[n];
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            while (stk.size() > 0 && arr[idx] <= arr[stk.peek()]) {
+                stk.pop();
+            }
+            res[idx] = (stk.size() > 0) ? stk.peek() : n;
+            stk.push(idx);
+        }
+
+        return res;
+    }
+
+    public int[] leftNSE(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stk = new Stack<>();
+        int[] res = new int[n];
+
+        for (int idx = 0; idx < n; idx++) {
+            while (stk.size() > 0 && arr[idx] < arr[stk.peek()]) {
+                stk.pop();
+            }
+            res[idx] = (stk.size() > 0) ? stk.peek() : -1;
+            stk.push(idx);
+        }
+
+        return res;
+    }
+}
