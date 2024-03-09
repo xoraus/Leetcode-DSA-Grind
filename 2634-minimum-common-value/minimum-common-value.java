1081695afd117efd39120a337078a540cd791e2b
Sajjad Salaria <imsafay@gmail.com>
2024-03-09 21:41:17 +0530

    Time: 5 ms (36.74%) | Memory: 63 MB (29.04%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int idx1 = 0, idx2 = 0;
+
+        while(idx1 < nums1.length && idx2 < nums2.length){
+            if(nums1[idx1] == nums2[idx2]){
+                return nums1[idx1];
+            } else if(nums1[idx1] < nums2[idx2]) {
+                idx1++;
+            } else if(nums1[idx1] > nums2[idx2]) {
+                idx2++;
+            } else {
+                idx1++;
+                idx2++;
+            }
+        }
+
+        return -1;
+    }
+}
